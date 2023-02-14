8871ed8d7d89b6ffb38851cbecc2a4791ca27924
bepbepimasheep <113216082+bepbepimasheep@users.noreply.github.com>
2023-02-14 14:52:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var mouseEvent = "empty";
+
+//Em JavaScript, primeiro armazene o elemento canvas dentro 
+//de uma variável e crie uma referência da variável canvas.
+canvas=
+document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color = "black";
+widthLine = 2;
+canvas.addEventListener("mousedown", myMouseDown);
+
+function myMouseDown(e) {
+color = document.getElementById("color").value;
+widthLine = document.getElementById("widthLine").value;
+radius = document.getElementById("radius").value;
+mouseEvent="mouseDown";
+}
+
+canvas.addEventListener("mousemove", myMouseMove);
+function myMouseMove(e) {
+    PositionMouseX = e.clientX - canvas.offsetLeft;
+    PositionMouseY = e.clientY - canvas.offsetTop;
+
+    if (mouseEvent == "mouseDown") {
+       ctx.beginPath();
+       ctx.strokeStyle = color;
+       ctx.lineWidth = widthLine;
+       ctx.arc(PositionMouseX,PositionMouseY, radius ,0 , 2 * Math.PI);
+       ctx.stroke();
+    }
+
+}
+
+canvas.addEventListener("mouseup", myMouseUp);
+function myMouseUp(e) {
+    mouseEvent = "mouseUP";
+}
+
+canvas.addEventListener("mouseleave", myMouseLeave);
+function myMouseLeave(e) {
+    mouseEvent = "mouseleave";
+}
+
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
